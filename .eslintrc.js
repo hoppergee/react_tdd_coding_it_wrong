c97d1b7ed3c8e055eba243a873cd270ece85e050
吉翔 <jx4554@gmail.com>
2020-03-03 00:13:00 +0800

    Fix other eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: [
     'codingitwrong',
   ],
-  parser: 'babel-eslint'
+  parser: 'babel-eslint',
+  plugins: [
+    'jest',
+  ],
+  env: {
+    'browser': true,
+    'jest/globals': true,
+  }
 };
