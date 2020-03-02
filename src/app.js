ba496deee2891aa004ecb2eead509bc31b7d1a05
吉翔 <jx4554@gmail.com>
2020-03-03 00:32:56 +0800

    Install @babel/plugin-proposal-class-properties

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  state = { foo: 'bar' }
+
   render() {
     return <div className="hello">Hello, world!</div>;
   }
