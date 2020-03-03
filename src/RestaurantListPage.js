7437ba634b7a9642915ae1d2e56e7150a188663a
吉翔 <jx4554@gmail.com>
2020-03-03 23:17:30 +0800

    Conditionally show new restaurant form

--- src/RestaurantListPage.js
+++ src/RestaurantListPage.js
@@ -4,10 +4,18 @@ import RestaurantList from './RestaurantList';
 
 export default class RestaurantListPage extends Component {
 
-  state = { restaurantNames: [] }
+  state = {
+    restaurantNames: [],
+    showNewRestaurantForm: false,
+  }
+
+  handleShowNewRestaurantForm = () => {
+    this.setState({ showNewRestaurantForm: true });
+  }
 
   handleAddRestaurant = (newRestaurantName) => {
     this.setState(state => ({
+      showNewRestaurantForm: false,
       restaurantNames: [
         newRestaurantName,
         ...state.restaurantNames,
@@ -16,17 +24,27 @@ export default class RestaurantListPage extends Component {
   }
 
   render() {
-    const { restaurantNames } = this.state
+    const {
+      restaurantNames,
+      showNewRestaurantForm
+    } = this.state
     return (
       <div>
         <button
           data-test="addRestaurantButton"
+          onClick={this.handleShowNewRestaurantForm}
         >
           Add Restaurant
         </button>
-        <NewRestaurantForm
-          onSave={this.handleAddRestaurant}
-        />
+        {
+          showNewRestaurantForm
+            ? (
+              <NewRestaurantForm
+                onSave={this.handleAddRestaurant}
+              />
+            )
+            : null
+        }
         <RestaurantList restaurantNames={restaurantNames} />
       </div>
     );
