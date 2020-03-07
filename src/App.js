09644f26902de8ebdb1afa0dd7830184a18613b3
吉翔 <jx4554@gmail.com>
2020-03-07 11:10:54 +0800

    Refactor to render app through React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import RestaurantListPage from './RestaurantListPage';
 import {
   Col,
@@ -8,11 +14,17 @@ import {
 export default class App extends React.Component {
   render() {
     return (
-      <Row>
-        <Col s={12} m={10} l={8} offset="m1 l2">
-          <RestaurantListPage />
-        </Col>
-      </Row>
+      <Router>
+        <Row>
+          <Col s={12} m={10} l={8} offset="m1 l2">
+            <Switch>
+              <Route path="/">
+                <RestaurantListPage />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Router>
     );
   }
 }
